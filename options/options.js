45641d1341dd4e40e76ccce068830a72ea994234
Crowdbotics <team@crowdbotics.com>
2023-07-02 17:45:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "calm-snow-41861",
+  url: "https://calm-snow-41861.botics.co",
+  api: "https://calm-snow-41861.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
